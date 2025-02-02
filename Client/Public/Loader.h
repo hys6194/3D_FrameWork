a9b40dcc7ca487f242f2d5c680d485723f393860
Hong.Ssi <ys01086506194@gmail.com>
2025-02-03 03:16:16 +0900

    Update Loader.h

--- Client/Public/Loader.h
+++ Client/Public/Loader.h
@@ -20,13 +20,19 @@ public:
 private:
 	ID3D11Device*					m_pDevice = { nullptr };
 	ID3D11DeviceContext*			m_pContext = { nullptr };
-	HANDLE							m_hThread = {};
-	CRITICAL_SECTION*				m_pCriticalSection = { nullptr };
-	LEVEL							m_eNextLevelID = { LEVEL_END };
+	
+	// 멀티 쓰레드의 작동을 위해서 필요한 두 멤버 변수임
+	HANDLE						m_hThread = {};
+	CRITICAL_SECTION			m_CriticalSection = {};
 
+	// 로더에서 어떤 레벨을 읽어올 지 알려주기 위해서 선언
+	LEVEL						m_eNextLevelID = { LEVEL_END };
+
+	// 로딩 레벨에서 글자를 읽어오기 위해서 만든 변수
 	_tchar							m_szLoading[MAX_PATH] = {};
-	_bool							m_bIsFin = { false };
 
+	// 레벨 전환을 위한 bool 타입의 변수
+	_bool							m_isFinished = { false };
 public:
 	static Loader* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, LEVEL eNextLevelID);
 	virtual void Free() override;
