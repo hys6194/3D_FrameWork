3dfc1f9804dce287c1abbc4c5ee49a9b943e3130
Hong.Ssi <ys01086506194@gmail.com>
2025-03-14 22:44:35 +0900

    Font, Manager 추가

---
+++ EngineSDK/Inc/Font_Manager.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Base.h"
+
+BEGIN(Engine)
+
+class CFont_Manager final :public CBase
+{
+private:
+	CFont_Manager(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual ~CFont_Manager() = default;
+
+public:
+	HRESULT Add_Font(const _wstring& strFontTag, const _tchar* pFontFilePath);
+	HRESULT Render(const _wstring& strFontTag, const _wstring& strText, const _float2& vPosition, _fvector vColor, _float fRadian, const _float2& vOrigin, _float fScale);
+
+private:
+	ID3D11Device* m_pDevice = { nullptr };
+	ID3D11DeviceContext* m_pContext = { nullptr };
+	map<const _wstring, class CMyFont*>				m_Fonts;
+
+private:
+	class CMyFont* Find_Font(const _wstring& strFontTag);
+
+
+public:
+	static CFont_Manager* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual void Free() override;
+};
+
+END
